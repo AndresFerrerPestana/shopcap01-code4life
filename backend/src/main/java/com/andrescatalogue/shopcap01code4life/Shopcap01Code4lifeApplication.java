e210557edef0e593217353ecb6298742d3bab65c
Andrés Ferrer <128063946+AndresFerrerPestana@users.noreply.github.com>
2023-06-15 13:18:00 +0100

    Backend project created

---
+++ backend/src/main/java/com/andrescatalogue/shopcap01code4life/Shopcap01Code4lifeApplication.java
@@ -0,0 +1,13 @@
+package com.andrescatalogue.shopcap01code4life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shopcap01Code4lifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shopcap01Code4lifeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andrescatalogue/shopcap01code4life/Shopcap01Code4lifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrescatalogue.shopcap01code4life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shopcap01Code4lifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
