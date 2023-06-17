970b71ced79dc9a1e4f574709fc8426886fcdd28
Andrés Ferrer <128063946+AndresFerrerPestana@users.noreply.github.com>
2023-06-17 13:55:46 +0100

    Insert Client

--- backend/src/main/java/com/andrescatalogue/shopcap01code4life/resources/ClientResource.java
+++ backend/src/main/java/com/andrescatalogue/shopcap01code4life/resources/ClientResource.java
@@ -1,14 +1,18 @@
 package com.andrescatalogue.shopcap01code4life.resources;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andrescatalogue.shopcap01code4life.dto.ClientDTO;
 import com.andrescatalogue.shopcap01code4life.services.ClientService;
@@ -61,5 +65,10 @@ public class ClientResource implements Serializable {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
